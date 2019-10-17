c44c776b6a04983716d3dc01ddfacd2d9e62ad5d
AbdullahNoori <nooriabdullah86@gmail.com>
2019-10-16 21:36:32 -0700

    added layout.html

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafter',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 21, 2019'
+    },
+    {
+        'author': 'Robin Wills',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'July 20, 2019'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__== '__main__':
+    app.run(debug=True)
